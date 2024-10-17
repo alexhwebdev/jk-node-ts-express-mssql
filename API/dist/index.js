ddfff770322c6e34d00e9444c6b8187179d75c67
Alex <alexhwebdev@gmail.com>
2024-10-16 14:02:51 -1000

    buildroutes

--- API/dist/index.js
+++ API/dist/index.js
@@ -6,17 +6,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = __importDefault(require("dotenv"));
-// import { appointmentRoute, diagnosisRoute, patientRoute, userRoute } from "./routes";
+const routes_1 = require("./routes");
 const app = (0, express_1.default)();
 dotenv_1.default.config();
 app.use((0, cors_1.default)());
 // app.use(json());
 app.use(express_1.default.json());
 const PORT = process.env.PORT;
-// app.use('/api/diagnosis',diagnosisRoute)
-// app.use('/api/users',userRoute);
-// app.use('/api/patients',patientRoute);
-// app.use('/api/appointments',appointmentRoute)
+app.use('/api/diagnosis', routes_1.diagnosisRoute);
+app.use('/api/users', routes_1.userRoute);
+app.use('/api/patients', routes_1.patientRoute);
+app.use('/api/appointments', routes_1.appointmentRoute);
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
